082b3790244f8aa99c50abf5e7dd31a2c5eef4f1
BaskaKlim <baska.klimekova@gmail.com>
2020-09-01 23:15:32 +0200

    using data.var() for variation and data.std() for standard deviation counting

---
+++ 13.The standard deviation and the variance.py
@@ -0,0 +1,13 @@
+# Compute the variance of the data in the versicolor_petal_length array using np.var() and store it in a variable called variance.
+# Print the square root of this value.
+# Print the standard deviation of the data in the versicolor_petal_length array using np.std().
+
+# Compute the variance: variance
+variance = np.var(versicolor_petal_length)
+
+# Print the square root of the variance
+print(np.sqrt(variance))
+
+# Print the standard deviation
+print(np.std(versicolor_petal_length))
+
